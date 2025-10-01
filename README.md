f91e362f5cf39e6bd3352f5a96d461fe1d78c087
IvanRizal <ivanrizal36@gmail.com>
2025-10-01 10:08:32 +0700

    Praktikum 2 - JS 6

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+![alt text](image.png)
 # 📱 Flutter Layout – Jobsheet 6
 
 Project ini merupakan hasil praktikum Flutter tentang **Layout** menggunakan widget dasar seperti `Row`, `Column`, `Expanded`, `Container`, `Text`, dan `Icon`.
@@ -16,6 +17,32 @@ Soal 3: Tambahkan Icon(Icons.star, color: Colors.red) dan teks "41" di sisi kana
 
 Untuk output nya adalah sebagai berikut:
 ![output 1](assets/image-1.png)
+
+# 📄 Praktikum 2: Implementasi Button Row
+![button row](image-1.png)
+Pada praktikum ini saya menambahkan baris tombol (button row) di bawah title section. Baris tombol berisi 3 buah kolom dengan tata letak yang sama:
+Sebuah ikon
+Sebuah teks di bawah ikon
+Karena bentuknya sama, maka dibuat sebuah method helper bernama _buildButtonColumn() yang menerima parameter:
+color → warna teks dan ikon
+icon → ikon yang ditampilkan
+label → teks di bawah ikon
+![helper](image-2.png)
+
+Soal 1: Mengapa dibuat method _buildButtonColumn?
+Supaya kode lebih rapi dan tidak perlu menulis berulang kali untuk setiap tombol.
+Metode ini mengembalikan sebuah Column yang berisi ikon dan teks.
+
+Soal 2: Bagaimana cara memberi jarak antara ikon dan teks?
+Teks diletakkan di dalam Container dengan margin: EdgeInsets.only(top: 8).
+Margin ini memberi jarak 8 piksel di atas teks, sehingga teks tidak menempel dengan ikon.
+
+Soal 3: Bagaimana cara meratakan tombol di dalam baris?
+Baris tombol dibuat dengan Row().
+Properti mainAxisAlignment: MainAxisAlignment.spaceEvenly digunakan agar semua kolom diberi jarak yang sama di sepanjang baris.
+
+Untuk output nya sebagai berikut
+![output prak 2](image-3.png)
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Title Section
     Widget titleSection = Container(
-      padding: const EdgeInsets.all(32), 
+      padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
-            /* soal 1 */
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, 
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /* soal 2 */
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8), 
+                  padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
                     'Wisata Gunung di Batu',
                     style: TextStyle(
-                      fontWeight: FontWeight.bold, 
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 const Text(
                   'Batu, Malang, Indonesia',
                   style: TextStyle(
-                    color: Colors.grey, 
+                    color: Colors.grey,
                   ),
                 ),
               ],
             ),
           ),
-          /* soal 3 */
           const Icon(
             Icons.star,
-            color: Colors.red, 
+            color: Colors.red,
           ),
           const Text('41'),
         ],
       ),
+    );
+
+    // Button Section
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
 
     return MaterialApp(
@@ -51,10 +61,35 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Center(
-          child: titleSection, 
+        body: Column(
+          children: [
+            titleSection,
+            buttonSection,
+          ],
         ),
       ),
+    );
+  }
+
+  // Method Helper
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
